865f8f1574a004d90f831cbdb596fb5c408ecbcd
glenn schrooyen <gschrooyen@gmail.com>
2020-03-03 10:18:55 +0100

    start project

---
+++ src/main/java/be/gschrooyen/restaurantdag/RestaurantdagOrganizerApplication.java
@@ -0,0 +1,13 @@
+package be.gschrooyen.restaurantdag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantdagOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantdagOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/gschrooyen/restaurantdag/RestaurantdagOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gschrooyen.restaurantdag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantdagOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
